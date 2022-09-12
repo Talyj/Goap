1ffdb9ef074d618f2281eeabb0dce459d466535f
Kyllian Marie-Magdelaine <77273553+kylyon@users.noreply.github.com>
2022-09-12 16:02:05 +0200

    Main test

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+#define NB_MAISON_FINAL = 15;
+
+int nbMaison = 0;
+int nbVillageois = 15;
+int nbBois = 0;
+int nbPierre = 0;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "World State : \{ Nombre de maison = " << nbMaison << " - Nombre de villageois = " << nbVillageois << " - Nombre de pierre = " << nbPierre << " - Nombre de bois = " << nbBois << " \}" << endl;
     return 0;
 }
